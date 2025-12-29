3f29bc0763fdcf2fe37a1819d3ba0fdcef05a534
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-29 18:16:32 +0500

    Pages and Routes Definintion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Appointment from './pages/Appointment'
+import Register from './pages/Register'
+import About from './pages/About'
+import Login from './pages/Login'
+
+
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/appointment' element={<Appointment />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Router>
     </>
