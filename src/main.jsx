3265ad6225520189d3a19c108567cecc87808794
Faisal Altaf <altaffaisal532@gmail.com>
2025-12-29 17:57:56 +0500

    Initial Setup for FrontEnd

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { createContext, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { useState } from 'react'
+
+
+export const Context = createContext({isAuthenticated: false});
+
+const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
+
+  return(
+    <Context.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}>
+      <App />
+    </Context.Provider>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppWrapper />
+  </React.StrictMode>,
+)
